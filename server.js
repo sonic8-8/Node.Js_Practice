4cd5810b9e3e886004881de1b0185084e9d1504a
이승환 <seaung13@gmail.com>
2024-07-23 16:35:06 +0900

    MongoDB 연결 및 환경 변수를 이용한 URL 숨김처리

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-app.listen(8080, () => {
-    console.log('http://www.localhost:8080에서 서버 실행 중')
+
+app.use(express.static(__dirname + '/public'));
+
+// ----------------------------------------------------------
+
+// 여기서 서버를 실행시키지말고 DB 연결 성공하면 실행 ㄱㄱ
+// app.listen(8080, () => {
+//     console.log('http://www.localhost:8080에서 서버 실행 중');
+// })
+
+// ----------------------------------------------------------
+
+// MongoDB 연결 세팅
+
+const { MongoClient } = require('mongodb');
+
+let db;
+const url = process.env.MONGODB_URL;
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공');
+    db = client.db('forum');
+
+    // 서버 실행
+
+    app.listen(8080, () => {
+        console.log('http://www.localhost:8080에서 서버 실행 중');
+    })
+
+
+}).catch((err) => {
+    console.log(err)
 })
 
 app.get('/', (요청, 응답) => {
